27136de9aa4354ff06e5152f9e6b0f77e7102334
Troy Prater <tprater93@gmail.com>
2023-08-12 00:19:58 -0500

    Complete

--- tictactoe/web/tictactoefront/src/App.js
+++ tictactoe/web/tictactoefront/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-import {Box} from './components/box.js';
+import { Board } from './components/board';
+import React, {useState} from "react";
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true);
+
+  const handleBoxClick = (boxIdx) => {
+    const updatedBoard = board.map((value, idx) => {
+      if(idx === boxIdx) {
+        return xPlaying === true ? "X" : "O"
+      } else {
+        return value;
+      }
+    })
+
+    setBoard(updatedBoard);
+
+    setXPlaying(!xPlaying);
+  }
+  
   return (
     <div classNAme="App">
-      <Box value="x" onClick={null}/>
+      <Board board={board} onClick={handleBoxClick}/>
     </div>
   );
 }
